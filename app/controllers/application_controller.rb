1ad4a5948ad33828edfa9885310852553491b90e
Liam Barstad <liambarstad@gmail.com>
2017-10-11 15:55:40 -0600

    successfully get bearer token

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,11 +5,21 @@ class ApplicationController < ActionController::Base
 
   def format_bearer_token_request
     Faraday.new(url: "https://www.reddit.com/api/v1/access_token") do |req|
+      req.request :multipart
+      req.adapter :net_http
       req.basic_auth(ENV["reddit_client_key"], ENV["reddit_secret_key"])
-      req.body.merge!({ grant_type: "authorization_code",
-                        code: session[:code],
-                        redirect_uri: "http://localhost:3000/login" })
+      req.params = { grant_type: "authorization_code",
+                   code: session[:code],
+                   redirect_uri: "http://localhost:3000/login" }
     end
   end
+
+  # def send_bearer_token_request(faraday_object)
+  #   faraday_object.post do |req|
+  #     req.body = { data: { grant_type: "authorization_code",
+  #                  code: session[:code],
+  #                  redirect_uri: "http://localhost:3000/login" }}
+  #   end
+  # end
 
 end
--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -3,8 +3,9 @@ class DashboardController < ApplicationController
 
   def index
     conn = format_bearer_token_request
-    user_info = conn.post("/me").body
-    binding.pry
+    response = conn.post
+    bearer_token = JSON.parse(response.body)
+    @bearer = BearerToken.create(bearer_token)
   end
 
 end
